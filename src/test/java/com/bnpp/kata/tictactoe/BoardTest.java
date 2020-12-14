2f47425bdab268340ef3a9b4bdfdb2d888140eaf
Prateek Agrawal <pratagra007@gmail.com>
2020-12-14 16:21:01 +0530

    Refactor: updated the test cases to have beforeeach implementation

--- src/test/java/com/bnpp/kata/tictactoe/BoardTest.java
+++ src/test/java/com/bnpp/kata/tictactoe/BoardTest.java
@@ -1,16 +1,22 @@
 package com.bnpp.kata.tictactoe;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
+    private Board board;
+
+    @BeforeEach
+    void initSetup() {
+        board = new Board();
+    }
 
     @Test
     @DisplayName("Should display plain board structure")
     void showBoard() {
-        Board board = new Board();
         String structure = "_ | _ | _";
         assertTrue(board.showBoard().contains(structure));
     }
@@ -18,7 +24,6 @@ class BoardTest {
     @Test
     @DisplayName("Should switch player accordingly")
     void switchPlayer() {
-        Board board = new Board();
         assertTrue(board.getCurrentPlayer().equals("X"));
         board.switchPlayer();
         assertTrue(board.getCurrentPlayer().equals("O"));
