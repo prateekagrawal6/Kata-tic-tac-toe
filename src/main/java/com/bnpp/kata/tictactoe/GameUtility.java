4f8dcc318fde496095713facd19f4d624574528c
Prateek Agrawal <pratagra007@gmail.com>
2020-12-14 17:41:18 +0530

    Added validation rule if the position is already occupied and to check all the positions are filled

--- src/main/java/com/bnpp/kata/tictactoe/GameUtility.java
+++ src/main/java/com/bnpp/kata/tictactoe/GameUtility.java
@@ -1,10 +1,20 @@
 package com.bnpp.kata.tictactoe;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class GameUtility {
 
     static boolean isValidInput(String input) {
-        return Arrays.stream(new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"}).anyMatch(x -> x.equals(input));
+        return Arrays.asList(new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"}).contains(input);
     }
+
+    static boolean isPositionOccupied(List<String> moves, String position){
+        return moves.contains(position);
+    }
+
+    static boolean isBoardGridMovesExhausted (List<String> moves ) {
+        return moves.size() > 8;
+    }
+
 }
--- src/test/java/com/bnpp/kata/tictactoe/GameUtilityTest.java
+++ src/test/java/com/bnpp/kata/tictactoe/GameUtilityTest.java
@@ -3,6 +3,9 @@ package com.bnpp.kata.tictactoe;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameUtilityTest {
@@ -14,5 +17,32 @@ class GameUtilityTest {
         assertTrue(GameUtility.isValidInput(input));
         String anotherInput = "a";
         assertFalse(GameUtility.isValidInput(anotherInput));
+    }
+
+    @Test
+    @DisplayName("Should check if the input given is already present or occupied")
+    void isPositionOccupied() {
+        List<String> moves = new ArrayList<String>();
+        moves.add("1");
+        moves.add("2");
+        assertTrue(GameUtility.isPositionOccupied(moves,"1"));
+        assertFalse(GameUtility.isPositionOccupied(moves,"3"));
+    }
+
+    @Test
+    @DisplayName("Should check if all the positions are exhausted in the board grid")
+    void isBoardGridMovesExhausted() {
+        List<String> moves = new ArrayList<String>();
+        moves.add("1");
+        moves.add("2");
+        moves.add("3");
+        moves.add("4");
+        moves.add("5");
+        moves.add("6");
+        moves.add("7");
+        moves.add("8");
+        assertFalse(GameUtility.isBoardGridMovesExhausted(moves));
+        moves.add("9");
+        assertTrue(GameUtility.isBoardGridMovesExhausted(moves));
     }
 }
