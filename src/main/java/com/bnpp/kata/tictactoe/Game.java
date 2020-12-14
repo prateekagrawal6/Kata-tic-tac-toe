ad48c48dc52a37d600351f3683369173402cf2fb
Prateek Agrawal <pratagra007@gmail.com>
2020-12-14 19:36:40 +0530

    Added play logic to allow user to give console input

--- src/main/java/com/bnpp/kata/tictactoe/Game.java
+++ src/main/java/com/bnpp/kata/tictactoe/Game.java
@@ -1,12 +1,26 @@
 package com.bnpp.kata.tictactoe;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Game {
+    private static final Logger logger = Logger.getLogger(Game.class.getName());
+
     private String winner;
     private Board board;
 
     public Game() {
         board = new Board();
         winner = null;
+    }
+
+    void play() {
+        logger.info(board.showBoard());
+        Scanner scanner = new Scanner(System.in);
+        logger.info("Enter the position(1-9) for " + board.getCurrentPlayer() + " : ");
+        String inputPosition = scanner.next().trim();
+        board.placeInputAtBoardGrid(inputPosition);
+        logger.info(board.showBoard());
     }
 
     public String getWinner() {
--- src/test/java/com/bnpp/kata/tictactoe/GameTest.java
+++ src/test/java/com/bnpp/kata/tictactoe/GameTest.java
@@ -1,17 +1,36 @@
 package com.bnpp.kata.tictactoe;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+    Game game;
+
+    @BeforeEach
+    public void initSetup() {
+        game = new Game();
+    }
 
     @Test
     @DisplayName("Should initialise game variables or members")
     void initializeGame() {
-        Game game = new Game();
         assertNotNull(game.getBoard());
         assertNull(game.getWinner());
+    }
+
+    @Test
+    @DisplayName("Should allow user to give input")
+    void play() {
+        InputStream sysInBackup = System.in; // backup System.in to restore it later
+        ByteArrayInputStream in = new ByteArrayInputStream("1".getBytes());
+        System.setIn(in);
+        game.play();
+        assertTrue(game.getBoard().showBoard().contains("X | _ | _"));
     }
 }
